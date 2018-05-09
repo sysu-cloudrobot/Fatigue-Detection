6120ed04afc07d8581d2f5d6aabad6c7c6f22bfb
ishoping <2859098211@qq.com>
2018-05-09 20:35:17 +0800

    add ImgWindow.py and change fatigue_detection.py

--- Stage1/fatigue_detection.py
+++ Stage1/fatigue_detection.py
@@ -15,27 +15,111 @@ import time
 import dlib
 import cv2
 
-def sound_alarm(path):
+from ImgWindow import ImgWindow
+
+class Fatigue_detection:
+    MOUTH_AR_THRESH = 0.2
+    MOUTH_AR_CONSEC_FRAMES = 48
+
+    EYE_AR_THRESH = 0.2
+    EYE_AR_CONSEC_FRAMES = 48
+
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+    (mStart, mEnd) = (60, 68)
+
+    def __init__(self):
+        self.eye_counter = 0
+        self.mouth_counter = 0
+        self.alarm_on = False
+        self.ear = 0.0
+        self.mar = 0.0
+
+
+    def sound_alarm(self, path):
     # play an alarm sound
-    music = pyglet.resource.media('alarm.wav')
-    music.play()
-    pyglet.app.run()
+        music = pyglet.resource.media('alarm.wav')
+        music.play()
+        pyglet.app.run()
 
-def eye_aspect_ratio(eye):
-	# compute the euclidean distances between the two sets of
-	# vertical eye landmarks (x, y)-coordinates
-	A = dist.euclidean(eye[1], eye[5])
-	B = dist.euclidean(eye[2], eye[4])
+    def mouth_aspect_ratio(self, mouth):
+        # compute the euclidean distances between the two sets of
+        # vertical eye landmarks (x, y)-coordinates
+        A = dist.euclidean(mouth[1], mouth[7])
+        B = dist.euclidean(mouth[2], mouth[6])
+        C = dist.euclidean(mouth[3], mouth[5])
 
-	# compute the euclidean distance between the horizon
-	# eye landmark (x, y)-coordinates
-	C = dist.euclidean(eye[0], eye[3])
+        # compute the euclidean distance between the horizon
+        # eye landmark (x, y)-coordinates
+        D = dist.euclidean(mouth[0], mouth[4])
 
-	# compute the eye aspect ratio
-	ear = (A + B) / (2.0 * C)
+        # compute the eye aspect ratio
+        mar = (A + B + C) / (3 * D)
 
-	# return the eye aspect ratio
-	return ear
+        # return the eye aspect ratio
+        return mar
+
+    def eye_aspect_ratio(self, eye):
+        # compute the euclidean distances between the two sets of
+        # vertical eye landmarks (x, y)-coordinates
+        A = dist.euclidean(eye[1], eye[5])
+        B = dist.euclidean(eye[2], eye[4])
+
+        # compute the euclidean distance between the horizon
+        # eye landmark (x, y)-coordinates
+        C = dist.euclidean(eye[0], eye[3])
+
+        # compute the eye aspect ratio
+        ear = (A + B) / (2.0 * C)
+
+        # return the eye aspect ratio
+        return ear
+
+    def mouth_fatigue_detection(self, shape):
+        mouth = shape[Fatigue_detection.mStart:Fatigue_detection.mEnd]
+        self.mar = self.mouth_aspect_ratio(mouth)
+
+        if self.mar > Fatigue_detection.MOUTH_AR_THRESH:
+            return True
+        else:
+            return False
+
+    def eye_fatigue_detection(self, shape):
+        leftEye = shape[Fatigue_detection.lStart:Fatigue_detection.lEnd]
+        rightEye = shape[Fatigue_detection.rStart:Fatigue_detection.rEnd]
+        leftEAR = self.eye_aspect_ratio(leftEye)
+        rightEAR = self.eye_aspect_ratio(rightEye)
+
+        # average the eye aspect ratio together for both eyes
+        self.ear = (leftEAR + rightEAR) / 2.0
+
+        # check to see if the eye aspect ratio is below the blink
+        # threshold, and if so, increment the blink frame counter
+        if self.ear < Fatigue_detection.EYE_AR_THRESH:
+            return True
+        else:
+            return False
+
+    def fatigue_detection(self, shape):
+        if self.eye_fatigue_detection(shape):
+            self.eye_counter += 1
+        else:
+            self.eye_counter = 0
+            self.alarm_on = False
+        
+        if self.mouth_fatigue_detection(shape):
+            self.mouth_counter += 1
+        else:
+            self.mouth_counter = 0
+            self.alarm_on = False
+
+        if self.eye_counter > Fatigue_detection.EYE_AR_CONSEC_FRAMES or self.mouth_counter > Fatigue_detection.MOUTH_AR_CONSEC_FRAMES:
+            self.alarm_on = True
+    
+    def is_fatigue(self):
+        return self.alarm_on
+
+
 
 if __name__ == '__main__':
     try:
@@ -49,19 +133,9 @@ if __name__ == '__main__':
             help="index of webcam on system")
         args = vars(ap.parse_args())
         
-        # define two constants, one for the eye aspect ratio to indicate
-        # blink and then a second constant for the number of consecutive
-        # frames the eye must be below the threshold for to set off the
-        # alarm
-
-        # EYE_AR_THRESH = 0.3
-        EYE_AR_THRESH = 0.2
-        EYE_AR_CONSEC_FRAMES = 48
-
-        # initialize the frame counter as well as a boolean used to
-        # indicate if the alarm is going off
-        COUNTER = 0
-        ALARM_ON = False
+        fd = Fatigue_detection()
+        iwin = ImgWindow(args)
+        
 
         # initialize dlib's face detector (HOG-based) and then create
         # the facial landmark predictor
@@ -69,14 +143,10 @@ if __name__ == '__main__':
         detector = dlib.get_frontal_face_detector()
         predictor = dlib.shape_predictor("68 face landmarks.dat")
 
-        # grab the indexes of the facial landmarks for the left and
-        # right eye, respectively
-        (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-        (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
         # start the video stream thread
         print("[INFO] starting video stream thread...")
-        vs = VideoStream(src=args["webcam"]).start()
+        
         time.sleep(1.0)
 
         # loop over frames from the video stream
@@ -84,8 +154,8 @@ if __name__ == '__main__':
             # grab the frame from the threaded video file stream, resize
             # it, and convert it to grayscale
             # channels)
-            frame = vs.read()
-            frame = imutils.resize(frame, width=450)
+            frame = iwin.read()
+            #frame = imutils.resize(frame, width=450)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             # detect faces in the grayscale frame
@@ -99,70 +169,18 @@ if __name__ == '__main__':
                 shape = predictor(gray, rect)
                 shape = face_utils.shape_to_np(shape)
 
-                # extract the left and right eye coordinates, then use the
-                # coordinates to compute the eye aspect ratio for both eyes
-                leftEye = shape[lStart:lEnd]
-                rightEye = shape[rStart:rEnd]
-                leftEAR = eye_aspect_ratio(leftEye)
-                rightEAR = eye_aspect_ratio(rightEye)
+                fd.fatigue_detection(shape)
 
-                # average the eye aspect ratio together for both eyes
-                ear = (leftEAR + rightEAR) / 2.0
-
-                # compute the convex hull for the left and right eye, then
-                # visualize each of the eyes
-                leftEyeHull = cv2.convexHull(leftEye)
-                rightEyeHull = cv2.convexHull(rightEye)
-                cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-                cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-
-                # check to see if the eye aspect ratio is below the blink
-                # threshold, and if so, increment the blink frame counter
-                if ear < EYE_AR_THRESH:
-                    COUNTER += 1
-
-                    # if the eyes were closed for a sufficient number of
-                    # then sound the alarm
-                    if COUNTER >= EYE_AR_CONSEC_FRAMES:
-                        # if the alarm is not on, turn it on
-                        if not ALARM_ON:
-                            ALARM_ON = True
-
-                            # check to see if an alarm file was supplied,
-                            # and if so, start a thread to have the alarm
-                            # sound played in the background
-                            if "alarm" in args and args["alarm"] != "":
-                                t = Thread(target=sound_alarm,
-                                    args=(args["alarm"],))
-                                t.deamon = True
-                                t.start()
-
-                        # draw an alarm on the frame
-                        cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-                # otherwise, the eye aspect ratio is not below the blink
-                # threshold, so reset the counter and alarm
-                else:
-                    COUNTER = 0
-                    ALARM_ON = False
-
-                # draw the computed eye aspect ratio on the frame to help
-                # with debugging and setting the correct eye aspect ratio
-                # thresholds and frame counters
-                cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        
-            # show the frame
-            cv2.imshow("Frame", frame)
-            key = cv2.waitKey(1) & 0xFF
-        
-            # if the `q` key was pressed, break from the loop
-            if key == ord("q"):
+                iwin.draw(shape, fd.is_fatigue(), fd.mar, fd.ear)
+            
+            iwin.show()
+                
+            if iwin.is_close():
                 break
 
+        iwin.close()
+
         # do a bit of cleanup
-        cv2.destroyAllWindows()
-        vs.stop()
+        
     except rospy.ROSInterruptException:
         pass
